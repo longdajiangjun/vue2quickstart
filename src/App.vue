437e3ac40915f9655db53453671f1e80029a099e
bruce <liujj@tfedu.net>
2016-11-02 15:13:45 +0800

    ok

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
     <li><router-link :to="{ path: '/hello', activeClass: 'onactive'}" >hello</router-link></li>
     <li><router-link :to="{ path: '/table', activeClass: 'onactive'}" >Table</router-link></li>
     <li><router-link :to="{ path: '/table2', activeClass: 'onactive'}" >Table Service</router-link></li>
+    <li><router-link :to="{ path: '/table3', activeClass: 'onactive'}" >Table Dialog</router-link></li>
     <li><router-link :to="{ path: '/checkbox', activeClass: 'onactive'}" >Checkbox</router-link></li>
     <div class="container">
       <router-view></router-view>
--- src/components/elements/Table3Dialog.vue
+++ src/components/elements/Table3Dialog.vue
@@ -1,32 +1,35 @@
 <template>
-  <el-table
-    :data="tableData"
-    selection-mode="multiple"
-    @selectionchange="handleSelectionChange"
-    style="width: 100%">
-    <el-table-column
-      type="selection"
-      width="10">
-    </el-table-column>
-    <el-table-column
-      type="index"
-      width="10">
-    </el-table-column>
-    <el-table-column
-      property="difficulty"
-      label="难度"
-      width="10">
-    </el-table-column>
-    <el-table-column
-      property="descript"
-      label="知识点"
-      width="220">
-    </el-table-column>
-    <el-table-column
-      property="amount"
-      label="题量">
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-button type="text" @click.native="isVisible = true">打开嵌套表格的 Dialog</el-button>
+    <el-dialog title="收货地址" v-model="isVisible">
+      <el-table
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          type="selection"
+          width="10">
+        </el-table-column>
+        <el-table-column
+          type="index"
+          width="10">
+        </el-table-column>
+        <el-table-column
+          property="difficulty"
+          label="难度"
+          width="10">
+        </el-table-column>
+        <el-table-column
+          property="descript"
+          label="知识点"
+          width="220">
+        </el-table-column>
+        <el-table-column
+          property="amount"
+          label="题量">
+        </el-table-column>
+      </el-table>
+    </el-dialog>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -34,22 +37,25 @@
   import equationService from '../../api/equationService'
 
   export default {
-
+    props:{
+      dialogTableVisible : Boolean
+    },
     data() {
       return {
         tableData: [],
+        isVisible : this.dialogTableVisible,
         singleSelection: {}
       }
     },
     mounted(){
-      this.$nextTick(() => {
-        equationService.queryByTFCode(1,10,"RJXX02011901").then(tableData => this.tableData = tableData);
+      let self = this;
+      equationService.queryByTFCode(1, 10, "RJXX02011901").then(function (datas) {
+//          console.log("curPage",curPage);
+        self.tableData = datas.list;
       });
     },
     methods: {
-      handleSelectionChange(val) {
-        this.singleSelection = val;
-      }
+
     }
   }
 </script>
